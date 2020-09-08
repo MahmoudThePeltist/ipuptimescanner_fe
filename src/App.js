f0c57729f589f7a5e2165a3b4d7f3457498f11ac
Mahmoud Masters <mahmoudaburascea@gmail.com>
2020-09-08 12:57:18 +0200

    FEATURE: Basic app file setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 function App() {
+
+  var links_list = ['Home','CPE Map','Websites','Add'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavbarComponent title={"Outage Scanner"} links={ links_list }/>
   );
 }
 
