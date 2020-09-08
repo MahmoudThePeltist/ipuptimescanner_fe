865f62dcb04f6de7aaa0e1024c4a82896948abc5
Mahmoud Masters <mahmoudaburascea@gmail.com>
2020-09-08 13:35:00 +0200

    FEATURE: scafolding for main views

---
+++ src/views/add_item_view/add_item.view.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export class AddItemView extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <h1>Add Item View</h1>
+        );
+    }
+    
+}
---
+++ src/views/websites_view/websites.view.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export class WebsitesView extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return(
+            <h1>Websites View</h1>
+        );
+    }
+
+}
