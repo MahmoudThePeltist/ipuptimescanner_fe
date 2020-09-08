f5f698d33ee260b1068070f3a2b04db0e1e7f070
Mahmoud Masters <mahmoudaburascea@gmail.com>
2020-09-09 01:49:28 +0200

    FEATURE: A service to communicate with the clients and perform all CRUD operations

---
+++ src/services/clients.service.js
@@ -0,0 +1,30 @@
+import  React from 'react';
+import axios from 'axios';
+
+export class ClientsService {
+    
+    constructor() {
+        this.url = 'http://localhost:3500/api';
+        this.resource = 'clients';
+    }
+    
+    getClients(){
+        return axios.get(this.url + '/' + this.resource);
+    }
+    
+    getSpecificClient(id){
+        return axios.get(this.url + '/' + this.resource + '/' + id);
+    }
+    
+    postClient(data){
+        return axios.post(this.url + '/' + this.resource, data);
+    }
+    
+    updateClient(data, id){
+        return axios.put(this.url + '/' + this.resource + '/' + id, data);
+    }
+
+    deleteClient(id){
+        return axios.delete(this.url + '/' + this.resource + '/' + id);
+    }
+}
