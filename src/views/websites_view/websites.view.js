3671693d37f9cf9db1d3ae49b1283190e2d6bafd
Mahmoud Masters <mahmoudaburascea@gmail.com>
2020-09-09 15:04:41 +0200

    FEATURE: Custom table to show active and disabled websites and cpes.

--- src/views/websites_view/websites.view.js
+++ src/views/websites_view/websites.view.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Table } from 'react-bootstrap';
+import { Container, Row, Col, Table, Card, Button, Spinner } from 'react-bootstrap';
+
 import { ClientsService } from '../../services/clients.service';
 
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+ 
 export class WebsitesView extends Component {
 
     constructor(props) {
@@ -11,11 +15,31 @@ export class WebsitesView extends Component {
     }
 
     componentDidMount() {
-        console.log("Getting clients... ");
+        this.getClients();
+    }
+    
+    getClients() {
+        this.setState({ clientData: undefined });
         this.clientsService.getClients()
             .then(response => {
                 console.log("Get clients response: ", response);
-                this.setState({ clientData: response['data'] })
+                // response['data'] = response['data'].filter( client => client.type === 'website' );
+                this.setState({ clientData: response['data'] });
+            })
+            .catch(error => {
+                console.log("Something went wrong: ", error);
+            })
+    }
+
+    deleteWebsite(id) {
+        this.clientsService.deleteClient(id)
+            .then(response => {
+                console.log("Get clients response: ", response);
+                this.getClients();
+            })
+            .catch(error => {
+                console.log("Something went wrong: ", error);
+                this.getClients();
             })
     }
 
@@ -24,34 +48,39 @@ export class WebsitesView extends Component {
             <Container>
                 <Row>
                     <Col className="text-center my-5">
-                    <Table striped bordered hover variant="dark">
-                        <thead>
-                            <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Address</th>
-                            <th>Status</th>
-                            <th>Description</th>
-                            <th>Created At</th>
-                            <th>Updated At</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {this.state.clientData ? this.state.clientData.map(row => 
-                            <tr key={row.id}>
-                                <td>{ row.id }</td>
-                                <td>{ row.name }</td>
-                                <td>{ row.address }</td>
-                                <td>{ row.status }</td>
-                                <td>{ row.description }</td>
-                                <td>{ row.created_at }</td>
-                                <td>{ row.updated_at }</td>
-                            </tr>
-                        ) : 
-                        <h1>Loading...</h1>
-                         }
-                        </tbody>
-                    </Table>
+                    <Card className="p-3">
+                        <div className="display-5 py-3">Websites</div>
+                        {this.state.clientData ? 
+                        <Table className="py-5" striped hover>
+                            <thead>
+                                <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Address</th>
+                                <th>Status</th>
+                                <th>Description</th>
+                                <th>Created At</th>
+                                <th>Updated At</th>
+                                <th>Tools</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {this.state.clientData.map(row => 
+                                <tr key={row.id}>
+                                    <td>{ row.id }</td>
+                                    <td>{ row.name }</td>
+                                    <td>{ row.address }</td>
+                                    <td>{ row.status }</td>
+                                    <td>{ row.description }</td>
+                                    <td>{ row.created_at }</td>
+                                    <td>{ row.updated_at }</td>
+                                    <td><Button className="btn-sm" onClick={() => this.deleteWebsite(row.id)} variant="secondary"><FontAwesomeIcon icon={faTrash} /></Button></td>
+                                </tr>
+                            )}
+                            </tbody>
+                        </Table>
+                        : <div className="mx-auto text-center"><Spinner animation="border" /></div> }
+                    </Card>
                     </Col>
                 </Row>
             </Container>
